40acc211424e07935deb31ba26a98caf28f858a2
Alen Mulalic <34632586+outofcake@users.noreply.github.com>
2020-01-03 11:11:09 -0600

    first Commit

---
+++ IdleClicker/Assets/Scenes/Scripts/Store.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Store : MonoBehaviour
+{
+    int storeCount; 
+
+    void Start()
+    {
+        storeCount = 1;
+    }
+
+    void Update()
+    {
+        
+    }
+    public void BuyStoreOnClick ()
+    {
+        storeCount += 1; 
+    }
+}
