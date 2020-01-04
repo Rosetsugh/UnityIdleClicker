64502d6193fd97a6069f28e2a7743c701e76befe
Alen Mulalic <34632586+outofcake@users.noreply.github.com>
2020-01-04 17:31:24 -0600

    Hero Stats Scene

---
+++ IdleClicker/Assets/Scripts/HeroUI.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeroUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int strVal, intVal, dexVal, conVal;
+    private string heroName;
+    public Text heroNameBox; 
+    public Text strBox;
+    public Text intBox;
+    public Text dexBox;
+    public Text conBox;
+    public Text combatPowerBox; 
+    int combatPower; 
+
+    void Start()
+    {
+        combatPower = strVal + intVal + dexVal + conVal;
+        heroName = DetermineHeroName(combatPower);
+        heroNameBox.text = heroName;
+        strBox.text = strVal.ToString();
+        intBox.text = intVal.ToString();
+        dexBox.text = dexVal.ToString();
+        conBox.text = conVal.ToString();
+        combatPowerBox.text = "Combat Power: " + combatPower; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private string DetermineHeroName (int powah)
+    {
+        string name; 
+        if (powah < 1000)
+        {
+            name = "Rookie Hero";
+        }else
+        {
+            name = "Super Hero"; 
+        }
+        return name; 
+    }
+
+}
