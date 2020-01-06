6dd001deaf1a73c4159d8b73cee604bc8f3f128e
Alen Mulalic <34632586+outofcake@users.noreply.github.com>
2020-01-06 15:28:40 -0600

    xml LoadFile

---
+++ IdleClicker/Assets/Scripts/LoadGameData.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+
+public class LoadGameData : MonoBehaviour
+{
+    public TextAsset GameData; 
+
+    public void Start ()
+    {
+        Invoke("LoadData", .1f); 
+    }
+
+
+    public void LoadData ()
+    {
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.LoadXml(GameData.text);
+
+        XmlNodeList StoreList = xmlDoc.GetElementsByTagName("store"); 
+
+        foreach (XmlNode StoreInfo in StoreList)
+        {
+            Debug.Log(StoreInfo.InnerText);
+            Debug.Log(StoreInfo.Name);
+        }
+    }
+}
