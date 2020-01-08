afedb7096495acc23e868fa59b0395b1dfa7fb20
Alen Mulalic <34632586+outofcake@users.noreply.github.com>
2020-01-07 19:36:53 -0600

    created all panels

--- IdleClicker/Assets/Scripts/SceneLoader.cs
+++ IdleClicker/Assets/Scripts/SceneLoader.cs
@@ -8,11 +8,13 @@ public class SceneLoader : MonoBehaviour
 {
 
     public GameObject idlePanel;
-    public GameObject characterPanel; 
+    public GameObject characterPanel;
+    public GameObject battlePanel;
 
     void Start ()
     {
         characterPanel.gameObject.SetActive(true);
+        battlePanel.gameObject.SetActive(false);
         idlePanel.gameObject.SetActive(false);
     }
 
@@ -20,12 +22,22 @@ public class SceneLoader : MonoBehaviour
     {
         characterPanel.gameObject.SetActive(true);
         idlePanel.gameObject.SetActive(false);
+        battlePanel.gameObject.SetActive(false);
     }
     public void LoadIdlePanel ()
     {
         characterPanel.gameObject.SetActive(false);
         idlePanel.gameObject.SetActive(true);
+        battlePanel.gameObject.SetActive(false);
     }
+    public void LoadBattlePanel ()
+    {
+        characterPanel.gameObject.SetActive(false);
+        idlePanel.gameObject.SetActive(false);
+        battlePanel.gameObject.SetActive(true);
+    }
+
+
     public void Quit ()
     {
         Application.Quit();
